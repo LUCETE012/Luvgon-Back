1e5ac0a868ae81826837cf7d31755fdc4f265b44
yeojin <yeojin7010+L@gmail.com>
2024-01-13 02:47:59 +0900

    fix: login

--- main.js
+++ main.js
@@ -8,8 +8,8 @@
  *
  */
 
-const express = require("express");
-const axios = require("axios");
+const express = require('express');
+const axios = require('axios');
 const { authConfig } = require('./config/config');
 const { sql, poolPromise } = require('./config/server');
 const { SqlConnector } = require('./db');
@@ -20,86 +20,87 @@ let sqlConn;
 
 // Enable CORS for all routes
 app.use(async (_, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
 });
 
 app.get('/test', async (req, res) => {
-  sqlConn.setUser('nsun527@cau.ac.kr');
-  const result = await sqlConn.getFollowings();
-  res.send(result);
+    sqlConn.setUser('nsun527@cau.ac.kr');
+    const result = await sqlConn.getFollowings();
+    res.send(result);
 });
-
 
 //루트 페이지
 //로그인 버튼을 누르면 GET /login으로 이동
-app.get("/", (_, res) => {
-  res.send(`<a href="/auth">Log in</a>
+app.get('/', (_, res) => {
+    res.send(`<a href="/auth">Log in</a>
   <a href="/signup">Sign up</a>`);
 });
 
 // 로그인 버튼을 누르면 도착하는 목적지 라우터
 // 모든 로직을 처리한 뒤 구글 인증서버인 https://accounts.google.com/o/oauth2/v2/auth
 // 으로 redirect 되는데, 이 url에 첨부할 몇가지 QueryString들이 필요
-app.get("/auth", (_, res) => {
-  res.redirect(`https://accounts.google.com/o/oauth2/v2/auth
-?client_id=${authConfig.clientID}
-&redirect_uri=${authConfig.redirectUri}
-&response_type=code
-&scope=email profile`);
+app.get('/auth', (_, res) => {
+    let url = 'https://accounts.google.com/o/oauth2/v2/auth';
+    url += `?client_id=${authConfig.clientID}`;
+    url += `&redirect_uri=${authConfig.redirectUri}`;
+    url += '&response_type=code';
+    url += '&scope=email profile';
+    res.redirect(url);
 });
 
-app.get("/auth/google", async (req, res) => {
-  const { code } = req.query;
-  console.log(`code: ${code}`);
+app.get('/auth/google', async (req, res) => {
+    const { code } = req.query;
+    console.log(`code: ${code}`);
 
-  const resp = await axios.post(authConfig.tokenUrl, {
-    code,
-    client_id:      authConfig.clientID,
-    client_secret:  authConfig.clientSecret,
-    redirect_uri:   authConfig.redirectUri,
-    grant_type:     "authorization_code",
-  });
+    const resp = await axios.post(authConfig.tokenUrl, {
+        code,
+        client_id: authConfig.clientID,
+        client_secret: authConfig.clientSecret,
+        redirect_uri: authConfig.redirectUri,
+        grant_type: 'authorization_code',
+    });
 
-  const resp2 = await axios.get(authConfig.userinfoUrl, {
-    headers: { Authorization: `Bearer ${resp.data.access_token}` },
-  });
-  res.json(resp2.data);
+    const resp2 = await axios.get(authConfig.userinfoUrl, {
+        headers: { Authorization: `Bearer ${resp.data.access_token}` },
+    });
+    res.json(resp2.data['email']);
 });
 
-app.get("/signup", (req, res) => {
-  res.redirect(`https://accounts.google.com/o/oauth2/v2/auth
-?client_id=${authConfig.clientID}
-&redirect_uri=${authConfig.signupUri}
-&response_type=code
-&scope=email profile`);
+app.get('/signup', (req, res) => {
+    let url = 'https://accounts.google.com/o/oauth2/v2/auth';
+    url += `?client_id=${authConfig.clientID}`;
+    url += `&redirect_uri=${authConfig.signupUri}`;
+    url += '&response_type=code';
+    url += '&scope=email profile';
+    res.redirect(url);
 });
 
-app.get("/signup/google", async (req, res) => {
-  const { code } = req.query;
-  console.log(`code: ${code}`);
+app.get('/signup/google', async (req, res) => {
+    const { code } = req.query;
+    console.log(`code: ${code}`);
 
-  const resp = await axios.post(authConfig.tokenUrl, {
-    code,
-    client_id:      authConfig.clientID,
-    client_secret:  authConfig.clientSecret,
-    redirect_uri:   authConfig.signupUri,
-    grant_type:     "authorization_code",
-  });
+    const resp = await axios.post(authConfig.tokenUrl, {
+        code,
+        client_id: authConfig.clientID,
+        client_secret: authConfig.clientSecret,
+        redirect_uri: authConfig.signupUri,
+        grant_type: 'authorization_code',
+    });
 
-  const resp2 = await axios.get(authConfig.userinfoUrl, {
-    headers: {
-      Authorization: `Bearer ${resp.data.access_token}`,
-    },
-  });
-  res.json(resp2.data);
+    const resp2 = await axios.get(authConfig.userinfoUrl, {
+        headers: {
+            Authorization: `Bearer ${resp.data.access_token}`,
+        },
+    });
+    res.json(resp2.data);
 });
 
 // Run server
 app.listen(PORT, async () => {
-  sqlConn = new SqlConnector(await poolPromise);
-  console.log("Connected to TastyNav database.");
-  console.log(`Listening to port ${PORT}...`);
+    sqlConn = new SqlConnector(await poolPromise);
+    console.log('Connected to TastyNav database.');
+    console.log(`Listening to port ${PORT}...`);
 });
